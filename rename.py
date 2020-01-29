1fd9b309f997b78947da3bfdb3ecfc183959d2d9
momokame <taro@es.ise.mail.kyutech.ac.jp>
2020-01-30 01:24:41 +0900

    sign_test

--- rename.py
+++ rename.py
@@ -1,8 +1,8 @@
 import os
 import glob
 from PIL import Image
-path1=('sign_test_pic')
-path2=('/mnt/500GBHDD/taro/plate/pic_32/no_plate_32')
+path1=('sign_test')
+path2=('sign_test2')
 files = glob.glob(os.path.join(path1,'*.png'))
 files2 = glob.glob(os.path.join(path2,'*.png'))
 i =1
@@ -18,7 +18,7 @@ for n, t in enumerate(files):
     img = Image.open(t)
     #img = Image.open('/mnt/500GBHDD/taro/Traffic_Signs/nosign_test/pst'+str(n+6000)+'.png')
     #img.save(os.path.join(path+'_bkup','img_'+'{0:03d}'.format(i)+'.png'))
-    img.save(os.path.join(path1,'p_'+'{0:03d}'.format(i)+'.png'))
+    img.save(os.path.join(path2,'p_'+'{0:04d}'.format(i)+'.png'))
     #t_00000_c5.png
     i=i+1
     if n == 3001:
